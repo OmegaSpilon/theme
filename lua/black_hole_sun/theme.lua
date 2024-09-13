c3b0027ebfdb71dc539867ea5f5b2e6340078929
OmegaSpilon <122259947+OmegaSpilon@users.noreply.github.com>
2024-09-13 22:30:38 +0530

    Update theme.lua

--- lua/black_hole_sun/theme.lua
+++ lua/black_hole_sun/theme.lua
@@ -325,7 +325,7 @@ theme.set_highlights = function()
   hl(0, "BufferInactiveTarget", { fg = c.red, bg = c.alt_bg, bold=true, })
 
   -- StatusLine
-  hl(0, "StatusLine", { fg = c.line, bg = c.grey })
+  hl(0, "StatusLine", { fg = c.line, bg = c.line_no })
   hl(0, "StatusLineNC", { fg = c.line, bg = c.grey })
   hl(0, "StatusLineSeparator", { fg = c.line, bg = 'NONE' })
   hl(0, "StatusLineTerm", { fg = c.line, bg = 'NONE' })
